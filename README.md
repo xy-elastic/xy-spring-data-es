9cb15bbab93f1df9f02dab8bcfc26b5127496008
winfuture <insoul_net@qq.com>
2019-10-28 13:06:50 +0800

    初始化spring-data-es工程

---
+++ README.md
@@ -0,0 +1,2 @@
+完成ES与spring data的简单整合示例
+可以参考这里的demo:https://github.com/spring-projects/spring-data-elasticsearch
---
+++ src/main/java/com/xywei/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.xywei.dao;
+
+/**
+ * @Description TODO
+ * @Author future
+ * @DateTime 2019/10/28 11:59
+ */
+public class EmployeeDao {
+}
